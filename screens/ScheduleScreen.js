9ba9d38835de3c787461881e726afc318e97c1a0
Alex Webster <17aw43@queensu.ca>
2020-09-30 00:52:25 -0400

    Committing work in progress schedule screen. Having troubles updating the state and imediately seeing the update

--- screens/ScheduleScreen.js
+++ screens/ScheduleScreen.js
@@ -1,82 +1,114 @@
-import { Ionicons } from "@expo/vector-icons";
+import { Ionicons, MaterialCommunityIcons } from "@expo/vector-icons";
 import * as WebBrowser from "expo-web-browser";
-import React from "react";
+import React, { useState, Component } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { RectButton, ScrollView } from "react-native-gesture-handler";
+import { LinearGradient } from "expo-linear-gradient";
+import Colors from "../constants/Colors";
+import moment from "moment";
+import { TouchableOpacity } from "react-native";
 
-export default function ScheduleScreen() {
-  return (
-    <ScrollView
-      style={styles.container}
-      contentContainerStyle={styles.contentContainer}
-    >
-      <OptionButton
-        icon="md-school"
-        label="Read the Expo documentation"
-        onPress={() => WebBrowser.openBrowserAsync("https://docs.expo.io")}
-      />
+export default class ScheduleScreen extends Component {
+  state = {
+    m: moment(),
+  };
+  render() {
+    console.log("rendering schedule");
+    const [m] = this.state;
 
-      <OptionButton
-        icon="md-compass"
-        label="Read the React Navigation documentation"
-        onPress={() =>
-          WebBrowser.openBrowserAsync("https://reactnavigation.org")
-        }
-      />
-
-      <OptionButton
-        icon="ios-chatboxes"
-        label="Ask a question on the forums"
-        onPress={() => WebBrowser.openBrowserAsync("https://forums.expo.io")}
-        isLastOption
-      />
-    </ScrollView>
-  );
-}
-
-function OptionButton({ icon, label, onPress, isLastOption }) {
-  return (
-    <RectButton
-      style={[styles.option, isLastOption && styles.lastOption]}
-      onPress={onPress}
-    >
-      <View style={{ flexDirection: "row" }}>
-        <View style={styles.optionIconContainer}>
-          <Ionicons name={icon} size={22} color="rgba(0,0,0,0.35)" />
+    var singleDayView = (x, y) => {
+      return (
+        <View>
+          <Text style={styles.day}>{x}</Text>
+          <Text style={styles.day}>{y}</Text>
         </View>
-        <View style={styles.optionTextContainer}>
-          <Text style={styles.optionText}>{label}</Text>
+      );
+    };
+
+    var daysSelectorContianer = () => {
+      var mn = m.clone();
+      var from_date = mn.startOf("week");
+      return (
+        <View
+          style={{
+            justifyContent: "space-evenly",
+            flexDirection: "row",
+            padding: 15,
+          }}
+        >
+          {singleDayView("S", from_date.format("D"))}
+          {singleDayView("M", from_date.add(1, "days").format("D"))}
+          {singleDayView("T", from_date.add(1, "days").format("D"))}
+          {singleDayView("W", from_date.add(1, "days").format("D"))}
+          {singleDayView("T", from_date.add(1, "days").format("D"))}
+          {singleDayView("F", from_date.add(1, "days").format("D"))}
+          {singleDayView("S", from_date.add(1, "days").format("D"))}
         </View>
-      </View>
-    </RectButton>
-  );
+      );
+    };
+
+    return (
+      <LinearGradient
+        colors={[Colors.loginPrimary, Colors.loginSecondary]}
+        style={styles.container}
+      >
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "center",
+          }}
+        >
+          <TouchableOpacity
+            onPress={() => {
+              console.log("heree");
+              m.add(-1, "weeks");
+            }}
+          >
+            <MaterialCommunityIcons name="arrow-left" color="white" size={30} />
+          </TouchableOpacity>
+          <Text style={styles.month}>{m.format("MMMM, YYYY")}</Text>
+          <MaterialCommunityIcons name="arrow-right" color="white" size={30} />
+        </View>
+        {daysSelectorContianer()}
+        <View style={styles.box}>
+          <Text style={styles.title}>Schedule</Text>
+        </View>
+      </LinearGradient>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fafafa",
+    justifyContent: "flex-end",
+    backgroundColor: "grey",
   },
-  contentContainer: {
-    paddingTop: 15,
+  box: {
+    backgroundColor: "white",
+    height: "70%",
+    borderTopLeftRadius: 50,
+    borderTopRightRadius: 50,
+    padding: 40,
   },
-  optionIconContainer: {
-    marginRight: 12,
+  title: {
+    fontFamily: "poppins-medium",
+    fontSize: 30,
+    color: Colors.header,
+    borderBottomColor: "#7CEAE2", // Add this to specify bottom border color
+    borderBottomWidth: 5,
   },
-  option: {
-    backgroundColor: "#fdfdfd",
-    paddingHorizontal: 15,
-    paddingVertical: 15,
-    borderWidth: StyleSheet.hairlineWidth,
-    borderBottomWidth: 0,
-    borderColor: "#ededed",
+  month: {
+    fontFamily: "poppins-medium",
+    fontSize: 25,
+    color: Colors.header,
+    paddingRight: 10,
+    paddingLeft: 10,
   },
-  lastOption: {
-    borderBottomWidth: StyleSheet.hairlineWidth,
-  },
-  optionText: {
-    fontSize: 15,
-    alignSelf: "flex-start",
-    marginTop: 1,
+  day: {
+    fontFamily: "poppins-medium",
+    fontSize: 20,
+    color: Colors.header,
+    paddingBottom: 10,
   },
 });
